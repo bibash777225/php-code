f30e6bae8f726f097bff58d7b2f2da65d4f88c01
bibash777225 <bibashghimire000@gmail.com>
2026-02-01 08:27:35 +0545

    feat:complete

--- unit7/ques15.php
+++ unit7/ques15.php
@@ -1,2 +1,22 @@
 <!--  Create a PageController class with a method getMessage(). Create a PageTemplate class with a
 method render($message). Use both classes to display a message like "Welcome to OOP in PHP!". -->
+<?php
+class PageController {
+    public function getMessage() {
+        return "Welcome to OOP in PHP!";
+    }
+}
+
+class PageTemplate {
+    public function render($message) {
+        echo $message;
+    }
+}
+
+// Using both classes
+$controller = new PageController();
+$template = new PageTemplate();
+
+$message = $controller->getMessage();
+$template->render($message);
+?>
